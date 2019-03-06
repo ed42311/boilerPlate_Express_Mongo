574bbbf6989f3dda0bed46c5bf9a816c2f65a1b6
Courtney Ott <courtneyott@Courtneys-MacBook-Pro-2.local>
2019-03-06 14:37:54 -0700

    Make Mongoose dream model

---
+++ models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dreamSchema = new mongoose.Schema({
+  title:{
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  }
+});
+
+const Dream = mongoose.model('Dream', dreamSchema);
+
+module.exports = {
+  Dream
+};
